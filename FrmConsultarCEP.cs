9dbf0f9c72d725a12d60dbfee89961ce255bbf49
“William <william.iso@hotmail.com>
2023-08-16 14:26:41 -0300

    Adicionar arquivos de projeto.

---
+++ FrmConsultarCEP.cs
@@ -0,0 +1,57 @@
+namespace ConsultarCEP
+{
+    public partial class FrmConsultarCEP : Form
+    {
+
+
+        public FrmConsultarCEP()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtEstado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCEP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCidade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBairro_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtRua_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
---
+++ Resultado.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace ConsultarCEP
+{
+    internal class Resultado
+    {
+
+        [JsonProperty(PropertyName = "cep")]
+        public string CEP { get; set; }
+
+        [JsonProperty(PropertyName = "logradouro")]
+        public string Logradouro { get; set; }
+
+        [JsonProperty(PropertyName = "complemento")]
+        public string Complemento { get; set; }
+
+        [JsonProperty(PropertyName = "bairro")]
+        public string Bairro { get; set; }
+
+        [JsonProperty(PropertyName = "localidade")]
+        public string Localidade { get; set; }
+
+       
+
+
+    }
+}
